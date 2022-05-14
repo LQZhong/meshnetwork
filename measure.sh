6b9451adeb826c3f63a0004b607b2ab445cad021
JulianSchmidt96 <69909913+JulianSchmidt96@users.noreply.github.com>
2022-05-14 13:42:49 +0200

    deleted bracket from echo

--- measure.sh
+++ measure.sh
@@ -1,24 +1,24 @@
-echo("starting iperf client")
-echo("..")
-echo(" ")
+echo "starting iperf client"
+echo ".."
+echo " "
 server_ip =$1
 
 iperf -V -c $server_ip  -i 1 -u -b 1G -t 1200 -l 10000  -r -y C > iperf_log_$HOSTNAME.csv &
-echo(" iperf is connected to $server_ip")
-echo(" ")
-echo(" ")
+echo " iperf is connected to $server_ip"
+echo " "
+echo " "
 
 
-echo("starting ping client")
-echo("..")
-echo(" ")
+echo  "starting ping client" 
+echo ".."
+echo " "
 
 ping -6 ff02::1%wlan0 -D > ping_log_$HOSTNAME.csv &
 
-echo(" ping is running")
-echo(" ")
-echo(" ")
+echo " ping is running" 
+echo " "
+echo " "
 
 
 tcpdump  -i wlan0  -s 0 -w dump_$HOSTNAME.pcap &
-echo(" tcpdump is running")
+echo " tcpdump is running"
