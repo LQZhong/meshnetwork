b75f411e8a99391c7a9438e102b126771d830216
JulianSchmidt96 <julianschmidtbewerbung@gmail.com>
2022-05-24 16:39:34 +0200

    addapted to minimize human error

--- measure.sh
+++ measure.sh
@@ -1,7 +1,32 @@
 
 
+# change date
+read -p "Enter date (YYYYMMDDHHmm): " dateinput 
+date -s $dateinput
 
+
+echo " running meshmerize link"
+echo " "
+echo "_________________________________"
+meshmerize link
+echo " "
+echo "_________________________________"
+echo ""
+echo " ipv6 neighbors are:"
+echo " "
+ip -6 neigh show
+echo " "
+echo "_________________________________"
+echo "  "
+echo " "
 read -p 'server-ip: ' server_ip
+echo " "
+echo "_________________________________"
+echo "  "
+echo " "
+read -p "set script run time" runtime
+echo "  "
+echo " "
 
 
 echo "starting iperf client"
@@ -9,13 +34,13 @@ echo ".."
 echo " "
 
 
-iperf -V -c $server_ip  -i 1 -u -b 1G -t 1200  -y C > iperf_log_$HOSTNAME_$(date +%Y-%m-%d-%H:%M:%S).csv &
+iperf -V -c $server_ip  -i 1 -u -b 1G -t $runtime -y C > iperf_log_$HOSTNAME_$(date +%Y-%m-%d-%H:%M:%S).csv &
 
 echo " iperf is connected to $server_ip"
 echo " "
 echo " "
-
-
+echo "_________________________________"
+echo " "
 echo  "starting ping client" 
 echo ".."
 echo " "
@@ -26,8 +51,9 @@ bash pingc -6 $server_ip  > ping_log_$HOSTNAME_$(date +%Y-%m-%d-%H:%M:%S).csv &
 echo " ping is running" 
 echo " "
 echo " "
-
-
+echo "_________________________________"
+echo " "
 tcpdump  -i wlan0  -s 0 -w dump_$HOSTNAME_$(date +%Y-%m-%d-%H:%M:%S).pcap &
-
+echo " "
 echo " tcpdump is running"
+echo " "
