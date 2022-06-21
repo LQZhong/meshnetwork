ca5c1145f675683529bebf557f07dd8361cf4ee6
JulianSchmidt96 <julianschmidtbewerbung@gmail.com>
2022-06-21 18:09:31 +0200

    create scenario dir

---
+++ measureOnServer.sh
@@ -0,0 +1,41 @@
+read -p "Enter date (YYYYMMDDHHmm): " dateinput 
+date -s $dateinput
+
+echo "date set to $dateinput"
+echo " "
+echo "_________________________________"
+
+read -p "Enter Scenario name" scenario
+
+mkdir -p $scenario # create directory for scenario if it doesn't allready exist
+cd $scenario
+
+
+echo " running meshmerize link"
+echo " "
+echo "_________________________________"
+meshmerize link
+echo " "
+echo "_________________________________"
+echo ""
+
+echo "_________________________________"
+echo "  "
+echo " "
+
+my_ip=$(getent hosts $(cat /etc/hostname) | awk '{print $1; exit}')
+
+echo " starting iperf server on $my_ip%wlan0"
+echo ".. "
+
+iperf -s -y C > server_log_$HOSTNAME_$(date +%Y-%m-%d-%H:%M:%S).csv &
+
+echo "_________________________________"
+echo " "
+echo " starting tcpdump"
+echo ".."
+echo " "
+tcpdump  -i wlan0  -s 0 -w dump_server_$HOSTNAME_$(date +%Y-%m-%d-%H:%M:%S).pcap &
+echo " "
+echo " tcpdump is running"
+echo " "
